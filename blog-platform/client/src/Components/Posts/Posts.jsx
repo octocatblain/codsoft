c4ee9376d0eb9ba490dade3cea942e7c74654323
octocatblain <blainmuema@gmail.com>
2024-01-24 22:26:24 +0300

    feat: change nothing

--- blog-platform/client/src/Components/Posts/Posts.jsx
+++ blog-platform/client/src/Components/Posts/Posts.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./Posts.css";
 
 const Posts = () => {
-  return <div className="posts__wrapper">Posts</div>;
+  return <div className="posts__wrapper">Posts part
+  </div>;
 };
 
 export default Posts;
