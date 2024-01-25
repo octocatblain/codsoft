7a4ab59bc346d32f65503483b8e01d2c88232145
octocatblain <blainmuema@gmail.com>
2024-01-26 02:06:13 +0300

    feat: add login page

--- blog-platform/client/src/App.js
+++ blog-platform/client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import BlogPost from "./Pages/BlogPost/BlogPost";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 import Settings from "./Pages/Settings/Settings";
 import WritePost from "./Pages/WritePost/WritePost";
 
@@ -12,7 +13,8 @@ function App() {
       {/* <Home /> */}
       {/* <BlogPost/> */}
       {/* <WritePost/> */}
-      <Settings/>
+      {/* <Settings/> */}
+      <Login/>
     </>
   );
 }
