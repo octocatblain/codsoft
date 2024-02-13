4d6aa964d13d158c035f1a7af6f7bf90a850a5a1
octocatblain <blainmuema@gmail.com>
2024-02-13 15:50:12 +0300

    fix: typography of about page

--- jobsearch/src/Pages/About/About.jsx
+++ jobsearch/src/Pages/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         <div className="about_hero">
           <div className="about_content">
             <h1 className="title">Empowering your Career Journey</h1>
-            <p className="text">At <span>JobSearch</span>, we believe that every individual deserves the opportunity to pursue a fulfilling career path. Our mission is simple: to empower job seekers like you with the tools, resources, and support needed to navigate the ever-evolving world of employment.</p>
+            <p className="text">At <span>JobSearch</span>, we believe that every individual deserves the opportunity to pursue a fulfilling career path. <em>Our mission is simple:</em> to empower job seekers like you with the tools, resources, and support needed to navigate the ever-evolving world of employment.</p>
           </div>
           <div className="about_image"></div>
         </div>
