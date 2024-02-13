cf822de45a503ec43fe7d98a324e37972e020fe3
octocatblain <blainmuema@gmail.com>
2024-02-13 21:53:36 +0300

    fix: adjust home page hero section image

--- jobsearch/src/Pages/Home/Home.jsx
+++ jobsearch/src/Pages/Home/Home.jsx
@@ -14,11 +14,11 @@ const Home = () => {
             <article className='hero_title'>
               <h1 className="title">
                 Welcome to <span className='job'>Job<span>Search</span></span> <br/><span className='subtitle'>Your Gateway to Career Success</span></h1>
-              <p className="text">Are you ready to take the next step in your career journey? Look no further than <span className='job'>Job<span>Search</span></span> – your ultimate destination for finding the perfect job match. Whether you're a seasoned professional or just starting out, <span className='job'>Job<span>Search</span></span> has the tools, resources, and opportunities to propel you towards your dream career.</p>
+              <p className="text">Are you ready to take the next step in your career journey? Look no further than <span className='job'>Job<span>Search</span></span> – your ultimate destination for finding the perfect job match. <br/><br/><br/>Whether you're a seasoned professional or just starting out, <span className='job'>Job<span>Search</span></span> has the tools, resources, and opportunities to propel you towards your dream career.</p>
             </article>
-            <div className="hero_image">
+            {/* <div className="hero_image">
               <img src="https://images.pexels.com/photos/392018/pexels-photo-392018.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-            </div>
+            </div> */}
           </div>
         </hero>
         <Search />
