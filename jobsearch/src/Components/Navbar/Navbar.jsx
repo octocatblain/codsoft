8906818ff4dd3ed25873d1846d4bff6eadd7b2d1
octocatblain <blainmuema@gmail.com>
2024-02-14 17:08:21 +0300

    fix: navabar links

--- jobsearch/src/Components/Navbar/Navbar.jsx
+++ jobsearch/src/Components/Navbar/Navbar.jsx
@@ -22,13 +22,13 @@ const Navbar = () => {
 
             <ul className="nav_menu">
                 <li className="nav_item"><Link to="/">Home</Link></li>
-                <li className="nav_item"><Link to="/companies">Companies</Link></li>
+                {/* <li className="nav_item"><Link to="/companies">Companies</Link></li> */}
                 <li className="nav_item"><Link to="/about">About</Link></li>
-                <li className="nav_item"><Link to="/blog">Blog</Link></li>
+                {/* <li className="nav_item"><Link to="/blog">Blog</Link></li> */}
                 <li className="nav_item"><Link to="/contact">Contact</Link></li>
-                <li className="nav_item"><Link to="/login">Login</Link></li>
-                <li className="nav_item"><Link to="/register">Register</Link></li>
-                {/* {isResponsive && (<BiXCircle className="icon close_icon" />)} */}
+                {/* <li className="nav_item"><Link to="/login">Login</Link></li> */}
+                {/* <li className="nav_item"><Link to="/register">Register</Link></li> */}
+                {isResponsive && (<BiXCircle className="icon close_icon" />)}
                 <BiXCircle className="icon close_icon" onClick={toggleResponsive} />
             </ul>
             <FaBars className="icon menu_icon" onClick={toggleResponsive}/>
