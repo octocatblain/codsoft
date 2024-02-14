39ba6c0d9eb9d71b3ba4439404ced76014c392d2
octocatblain <blainmuema@gmail.com>
2024-02-14 21:01:04 +0300

    feat: add modal

--- jobsearch/src/Components/Navbar/Navbar.jsx
+++ jobsearch/src/Components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                 <li className="nav_item"><Link to="/">Home</Link></li>
                 {/* <li className="nav_item"><Link to="/companies">Companies</Link></li> */}
                 <li className="nav_item"><Link to="/about">About</Link></li>
-                {/* <li className="nav_item"><Link to="/blog">Blog</Link></li> */}
+                <li className="nav_item"><Link to="/blog">Blog</Link></li>
                 <li className="nav_item"><Link to="/contact">Contact</Link></li>
                 {/* <li className="nav_item"><Link to="/login">Login</Link></li> */}
                 {/* <li className="nav_item"><Link to="/register">Register</Link></li> */}
--- jobsearch/src/Pages/Blog/Blog.jsx
+++ jobsearch/src/Pages/Blog/Blog.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import Layout from "../../Layout/Layout"
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+const MyModal = ({ isOpen, handleClose }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={handleClose}
+      contentLabel="My Modal"
+      style={customStyles}
+    >
+      <h2>My Modal</h2>
+      <button onClick={handleClose}>Close Modal</button>
+    </Modal>
+  );
+};
 
 const Blog = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Layout>
+      <button onClick={openModal}>Open Modal</button>
+      <MyModal isOpen={modalIsOpen} handleClose={closeModal} />
+    </Layout>
+  );
+};
 
 export default Blog
+
+
