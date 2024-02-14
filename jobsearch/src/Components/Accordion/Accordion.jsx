315764f5ee550d1df44e3a7f31b740b39032c0c1
octocatblain <blainmuema@gmail.com>
2024-02-14 21:02:29 +0300

    feat: add accordion component

---
+++ jobsearch/src/Components/Accordion/Accordion.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onItemClick = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isActive = index === activeIndex ? 'active' : '';
+
+    return (
+      <React.Fragment key={index}>
+        <div className={`title ${isActive}`} onClick={() => onItemClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`content ${isActive}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
+};
+
+export default Accordion;
